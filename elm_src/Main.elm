d18927800a62488cccfb03748690699a2a978d84
fxmy <fxmywc@gmail.com>
2016-12-19 00:08:29 +0800

    hello material design

--- elm_src/Main.elm
+++ elm_src/Main.elm
@@ -1,48 +1,98 @@
 module BlogFxmy.Main exposing (..)
 
-import Html exposing ( Html, div, text, program )
+import Html exposing (..)
+import Html.Attributes exposing (href, class, style)
+import Material
+import Material.Scheme
+import Material.Button as Button
+import Material.Layout as Layout
+import Material.Color as Color
+
 
 -- MODEL
+type alias Model = {
+  header_content : String,
+  mdl : Material.Model,
+  tab_selected : Int
+  }
 
-type alias Model =
-  String
+model : Model
+model = {
+  header_content = "No Gods or Kings, only Man.",
+  mdl = Material.model,
+  tab_selected = 0
+  }
 
 
-init: ( Model, Cmd Msg )
+-- INIT
+init : ( Model, Cmd Msg )
 init =
-    ( "Hello elm static site", Cmd.none )
+    ( model, Cmd.none )
 
 
 -- MESSAGES
-
 type Msg =
-  NoOp
+  Mdl ( Material.Msg Msg)
+    | SelectTab Int
+    | NoOp
 
 
 -- VIEW
-
-view: Model -> Html Msg
+view : Model -> Html Msg
 view model =
-  div [][ text model ]
+  Material.Scheme.topWithScheme Color.Teal Color.Pink <|
+    Layout.render Mdl model.mdl
+    [ Layout.waterfall True,
+      Layout.onSelectTab SelectTab,
+      Layout.selectedTab model.tab_selected]
+    { header = [ h1[ style[( "padding", "1rem")]] [ text model.header_content]],
+      drawer = [],
+      tabs = ( [ text "Blog", text "About"], [ Color.background ( Color.color Color.Teal Color.S400)]),
+      main = [ view_main model]
+    }
+
+-- VIEWMAIN
+view_main : Model -> Html Msg
+view_main model =
+  case model.tab_selected of
+    0 ->
+      view_blog model
+    1 ->
+      view_about model
+    _ ->
+      h3 [ style[ ("margin", "0 24px")]] [ text "404 UCCU σ`∀´)"]
+
+-- VIEWBLOG
+view_blog : Model -> Html Msg
+view_blog model =
+  h3 [ style[ ("margin", "0 24px")]] [ text "此乃蒹葭 (〃∀〃)"]
+
+
+-- VIEWABOUT
+view_about : Model -> Html Msg
+view_about model =
+  h2 [ style[ ("margin", "0 24px")]] [ text "fxmywc@gmail.com (`ε´ )"]
 
 -- UPDATE
-
-update: Msg -> Model -> ( Model, Cmd Msg )
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
   case msg of
     NoOp ->
       ( model, Cmd.none )
+    SelectTab num ->
+      { model | tab_selected = num} ! []
+    Mdl msg_mdl ->
+      Material.update msg_mdl model
 
 
 -- SUBSCRIPTIONS
-
-subscriptions: Model -> Sub Msg
+subscriptions : Model -> Sub Msg
 subscriptions model =
   Sub.none
 
 
 -- MAIN
-main: Program Never Model Msg
+main : Program Never Model Msg
 main =
   program {
     init = init,
